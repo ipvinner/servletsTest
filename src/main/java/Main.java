32e55c6b1bfed85329b593a0679207938e58e5a0
Ivan Vynogradov <vaniavinogradov@rambler.ru>
2016-01-17 14:43:23 +0200

    added nullzezo var

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@
  * Created by ipvinner on 17.01.2016.
  */
 public class Main {
+    private int nullzero = 0;
     public static void main(String[] args) {
         System.out.println("Git branch testing");
 
